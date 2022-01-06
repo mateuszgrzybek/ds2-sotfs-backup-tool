cd2132868869b8ef1bd86ed7a936ab2993c666bb
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2022-01-07 00:51:42 +0100

    Adjust paths for Windows. Add actual sampe path to sample file

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def handle_path_info(current_dir):
 
 def cleanup_backups():
     list_of_backups = os.listdir("backups")
-    abs_path = ["backups/{0}".format(i) for i in list_of_backups]
+    abs_path = ["backups\\{0}".format(i) for i in list_of_backups]
 
     if len(list_of_backups) == 10:
         print("Removing oldest backup...")
@@ -37,8 +37,8 @@ def cleanup_backups():
 
 
 def create_backup(save_file_path, backup_dir):
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H:%M:%S')
-    save_file_name = save_file_path.rsplit('/', 1)[-1]
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d.%H-%M-%S')
+    save_file_name = save_file_path.rsplit('\\', 1)[-1]
     cleanup_backups()
     print(f"Creating backup with timestamp: {timestamp}")
     copyfile(save_file_path, backup_dir+f"{save_file_name}.{timestamp}")
@@ -46,13 +46,20 @@ def create_backup(save_file_path, backup_dir):
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 save_file_path = handle_path_info(current_dir)
-backup_dir = current_dir+"/backups/"
+backup_dir = current_dir + "\\backups\\"
 
 if not os.path.isdir(backup_dir):
     os.mkdir(backup_dir)
 
-backup_interval = input(">>>How often do you want the tool to create a new backup? Please specify the value in MINUTES\n>>> ")
-schedule.every(int(backup_interval)).minutes.do(create_backup, save_file_path, backup_dir)
+while True:
+    try:
+        backup_interval = int(input(">>>How often do you want the tool to create a new backup? Please specify the value in MINUTES\n>>> "))
+    except ValueError:
+        print("You must specify the value as minutes in numeric format")
+    else:
+        break
+
+schedule.every(backup_interval).minutes.do(create_backup, save_file_path, backup_dir)
 atexit.register(handle_exit)
 
 while True:
