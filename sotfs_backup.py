0dded0d81ae6407beea6166e6bab1dbc6e5b4637
mateuszgrzybek <mateusz.grzybek06@gmail.com>
2022-01-07 13:15:02 +0100

    Rename main to sotfs_backup

--- sotfs_backup.py
+++ sotfs_backup.py
@@ -3,13 +3,12 @@ import atexit
 import schedule
 import time
 import datetime
-import sys
-from keyboard import is_pressed
 from shutil import copyfile
 
 
 def handle_exit():
-    os.system("taskkill /f /im DarkSoulsII.exe")
+    print("Killing DS2 process")
+    os.system("taskkill /f /im DS2.exe")
 
 
 def handle_path_info(current_dir):
@@ -29,7 +28,7 @@ def handle_path_info(current_dir):
 
 def cleanup_backups():
     list_of_backups = os.listdir("backups")
-    abs_path = ["backups\\{0}".format(i) for i in list_of_backups]
+    abs_path = ["backups/{0}".format(i) for i in list_of_backups]
 
     if len(list_of_backups) == 10:
         print("Removing oldest backup...")
@@ -38,8 +37,8 @@ def cleanup_backups():
 
 
 def create_backup(save_file_path, backup_dir):
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d.%H-%M-%S')
-    save_file_name = save_file_path.rsplit('\\', 1)[-1]
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H:%M:%S')
+    save_file_name = save_file_path.rsplit('/', 1)[-1]
     cleanup_backups()
     print(f"Creating backup with timestamp: {timestamp}")
     copyfile(save_file_path, backup_dir+f"{save_file_name}.{timestamp}")
@@ -47,27 +46,15 @@ def create_backup(save_file_path, backup_dir):
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 save_file_path = handle_path_info(current_dir)
-backup_dir = current_dir + "\\backups\\"
+backup_dir = current_dir+"/backups/"
 
 if not os.path.isdir(backup_dir):
     os.mkdir(backup_dir)
 
-while True:
-    try:
-        backup_interval = int(input(">>>How often do you want the tool to create a new backup? Please specify the value in MINUTES\n>>> "))
-    except ValueError:
-        print("You must specify the value as minutes in numeric format")
-    else:
-        break
-
-schedule.every(backup_interval).minutes.do(create_backup, save_file_path, backup_dir)
+backup_interval = input(">>>How often do you want the tool to create a new backup? Please specify the value in MINUTES\n>>> ")
+schedule.every(int(backup_interval)).minutes.do(create_backup, save_file_path, backup_dir)
 atexit.register(handle_exit)
 
 while True:
     schedule.run_pending()
-    
-    if is_pressed("esc"):
-        sys.exit()
-
     time.sleep(1)
-    
